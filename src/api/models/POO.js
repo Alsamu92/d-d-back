803f0da0aeed0a57adafd75e56a358ce36301227
Alex <alexsanzm@gmail.com>
2024-02-06 23:38:48 +0100

    controlador personjae

---
+++ src/api/models/POO.js
@@ -0,0 +1,21 @@
+
+
+
+const user={
+    name:"Pepe",
+    surname:"Sanz",
+    age:31,
+    sumAge(cant){
+       this.age=this.age+cant
+    }
+}
+
+
+console.log(user2.showName())
+
+// const returnName=(a,b)=>{
+//    console.log( `${a} ${b}`)
+// }
+
+// returnName(user.name,user.surname)
+
